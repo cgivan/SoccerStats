c0f96c147e938e1fd7e9d95db279a43f86dcfa90
Chris Givan <chrisgivan502@gmail.com>
2018-09-18 17:10:10 -0400

    Added GameResult class

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -13,10 +13,7 @@ namespace SoccerStats
 
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
             var fileContents = ReadSoccerResults(fileName);
-            
-
-            
-
+    
             //var file = new FileInfo(fileName);
             //if (file.Exists)
             //{
@@ -43,9 +40,17 @@ namespace SoccerStats
             using (var reader = new StreamReader(fileName))
             {
                 string line = "";
+                reader.ReadLine();
                 while((line = reader.ReadLine()) != null)
                 {
+                    var gameResult = new GameResult();
                     string[] values = line.Split(',');
+                    
+                    DateTime gameDate;
+                    if(DateTime.TryParse(values[0], out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
                     soccerResults.Add(values);
                 }
             }
