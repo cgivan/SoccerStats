ed7f5ed07760a0b91921f36e5549304b412271eb
Chris Givan <chrisgivan502@gmail.com>
2018-09-18 14:45:00 -0400

    Added List<string> to read soccer results csv

--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace SoccerStats
 {
@@ -11,12 +12,7 @@ namespace SoccerStats
             DirectoryInfo directory = new DirectoryInfo(currentDirctory);
 
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string[] fileLines = fileContents.Split(new char[] { '\r', '\n' },StringSplitOptions.RemoveEmptyEntries);
-            foreach(var line in fileLines)
-            {
-                Console.WriteLine(line);
-            }
+            var fileContents = ReadSoccerResults(fileName);
             
 
             
@@ -39,6 +35,21 @@ namespace SoccerStats
             {
                 return reader.ReadToEnd();
             }
+        }
+
+        public static List<string[]> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<string[]>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                while((line = reader.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    soccerResults.Add(values);
+                }
+            }
+            return soccerResults;
         }
     }
 }
